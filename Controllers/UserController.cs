0c7c841223482d190e5673a1caea772a0ee4f306
Katarzyna Kasprzak <katarzyna_kasprzak@next.co.uk>
2023-03-28 12:18:41 +0100

    Add project files.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InternProjectV3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class UserController : ControllerBase
+    {
+        private readonly ILogger<UserController> _logger;
+
+        public UserController(ILogger<UserController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            var list1 = new List<string>();
+
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
